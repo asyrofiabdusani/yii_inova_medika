0bcadc0d00ef6ea258a1b36d5153340892eb471a
asyrofiabdusani <asyrofiabdusani@gmail.com>
2022-10-09 15:59:47 +0700

    fix bug in harga/price

--- views/detail-berobat/_form.php
+++ views/detail-berobat/_form.php
@@ -11,38 +11,42 @@ use app\models\Tindakan;
 /** @var app\models\DetailBerobat $model */
 /** @var yii\widgets\ActiveForm $form */
 
-$obat = Obat::find()->asArray()->all();
-$value = ArrayHelper::getValue($obat, 'nama');
-$tindakan = ArrayHelper::map(Tindakan::find()->asArray()->all(),'id_tindakan', 'nama');
-
-var_dump($obat);
+// $obat = Obat::find()->asArray()->all();
+// $value = ArrayHelper::getValue($obat, 'nama');
+$tindakan = ArrayHelper::map(Tindakan::find()->asArray()->all(), 'id_tindakan', 'nama');
+$obat = ArrayHelper::map(Obat::find()->asArray()->all(), 'id_obat', 'nama');
+$tindakan1 = ArrayHelper::map(Tindakan::find()->all(), 'id_tindakan', 'harga');
 ?>
 
 <div class="detail-berobat-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?php if ($_GET['input']=='obat') : ?>
+    <?php if ($_GET['input'] == 'obat') : ?>
 
-        <?= $form->field($model, 'id_berobat')->textInput()->input('id_berobat',['value'=> $_GET['id'], 'style' => 'display:none;'])->label(false); ?>
+        <?= $form->field($model, 'id_berobat')->textInput()->input('id_berobat', ['value' => $_GET['id'], 'style' => 'display:none;'])->label(false); ?>
 
-        <?php //$form->field($model, 'id_tindakan')->textInput() ?>
+        <?php //$form->field($model, 'id_tindakan')->textInput() 
+        ?>
 
-        <?php //$form->field($model, 'id_obat')->textInput() ?>
+        <?php //$form->field($model, 'id_obat')->textInput() 
+        ?>
 
-        <?= $form->field($model, 'id_obat')->dropDownList(['1' => 'aaa', '2' => 'bbb'], ['prompt'=>'-- Pilih Obat --', 'class' => 'drop form-control'])?>
+        <?= $form->field($model, 'id_obat')->dropDownList($obat, ['prompt' => '-- Pilih Obat --', 'class' => 'drop form-control']) ?>
 
-        <?=$form->field($model, 'harga')->textInput()->input('harga', ['class' => 'price form-control']) ?>
+        <?= $form->field($model, 'harga')->textInput()->input('harga', ['class' => 'price form-control']) ?>
 
     <?php else : ?>
 
-        <?= $form->field($model, 'id_berobat')->textInput()->input('id_berobat',['value'=> $_GET['id'], 'style' => 'display:none;'])->label(false); ?>
+        <?= $form->field($model, 'id_berobat')->textInput()->input('id_berobat', ['value' => $_GET['id'], 'style' => 'display:none;'])->label(false); ?>
 
-        <?php //$form->field($model, 'id_obat')->textInput() ?>
+        <?php //$form->field($model, 'id_obat')->textInput() 
+        ?>
 
-        <?php //$form->field($model, 'id_tindakan')->textInput() ?>
+        <?php //$form->field($model, 'id_tindakan')->textInput() 
+        ?>
 
-        <?= $form->field($model, 'id_tindakan')->dropDownList($tindakan, ['prompt'=>'-- Pilih Tindakan --', 'class' => 'drop form-control']); ?>
+        <?= $form->field($model, 'id_tindakan')->dropDownList($tindakan, ['prompt' => '-- Pilih Tindakan --', 'class' => 'drop form-control']); ?>
 
         <?= $form->field($model, 'harga')->textInput()->input('harga', ['class' => 'price form-control']) ?>
 
@@ -54,13 +58,17 @@ var_dump($obat);
 
     <?php ActiveForm::end(); ?>
 
+
 </div>
 
 <script>
     const drop = document.querySelector('.drop');
     const harga = document.querySelector('.price');
 
-    drop.addEventListener('change', ()=> {
-        harga.setAttribute('value',drop.classList[0]);
+    let lisHarga = <?php echo json_encode($tindakan1); ?>;
+
+
+    drop.addEventListener('change', () => {
+        harga.setAttribute('value', lisHarga[drop.value]);
     });
 </script>
--- views/detail-berobat/index.php
+++ views/detail-berobat/index.php
@@ -22,18 +22,18 @@ use app\models\Obat;
 $this->title = 'Detail Berobat';
 // $this->params['breadcrumbs'][] = $this->title;
 
-$berobat= TransaksiBerobat::findOne(['id_berobat'=>$_GET['id']]);
-$pasien= Pasien::findOne(['id_pasien'=>$berobat->id_pasien]);
-$pegawai= Pegawai::findOne(['id_pegawai'=>$berobat->id_pegawai]);
-$ruangan= Wilayah::findOne(['id'=>$berobat->id_wilayah]);
-$penyakit= Penyakit::findOne(['id_penyakit'=>$berobat->id_penyakit]);
+$berobat = TransaksiBerobat::findOne(['id_berobat' => $_GET['id']]);
+$pasien = Pasien::findOne(['id_pasien' => $berobat->id_pasien]);
+$pegawai = Pegawai::findOne(['id_pegawai' => $berobat->id_pegawai]);
+$ruangan = Wilayah::findOne(['id' => $berobat->id_wilayah]);
+$penyakit = Penyakit::findOne(['id_penyakit' => $berobat->id_penyakit]);
 // $p = ArrayHelper::map(Detailberobat::findAll(['id_berobat'=>$_GET['id']]));
 
 $trx = Detailberobat::find()
-    ->where(['id_berobat'=>$_GET['id']])
+    ->where(['id_berobat' => $_GET['id']])
     ->all();
-if ($trx!=NULL) {
-    $obat= Tindakan::findOne(['id_tindakan'=>$trx[0]->id_tindakan]);
+if ($trx != NULL) {
+    $obat = Tindakan::findOne(['id_tindakan' => $trx[0]->id_tindakan]);
 }
 
 // var_dump($obat);
@@ -46,43 +46,44 @@ if ($trx!=NULL) {
     <div class="row">
         <div class="col-md-7">
             <p>
-                <?= Html::a('Input Obat', ['create?input=obat&id='.$_GET['id']], ['class' => 'btn btn-success']) ?>
+                <?= Html::a('Input Obat', ['create?input=obat&id=' . $_GET['id']], ['class' => 'btn btn-success']) ?>
 
-                <?= Html::a('Input Tindakan', ['create?input=tindakan&id='.$_GET['id']], ['class' => 'btn btn-success']) ?>
+                <?= Html::a('Input Tindakan', ['create?input=tindakan&id=' . $_GET['id']], ['class' => 'btn btn-success']) ?>
             </p>
         </div>
         <div class="col-md-5">
             <div class="d-flex flex-row-reverse">
                 <p>
-                    <?= Html::a('Print Laporan', ['create?input=obat&id='.$_GET['id']], ['class' => 'btn btn-info']) ?>
+                    <?= Html::a('Print Laporan', '', ['class' => 'btn btn-info']) ?>
                 </p>
             </div>
         </div>
     </div>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); 
+    ?>
 
     <table class="table my-3">
         <thead>
             <tr>
-            <th scope="col">Nama Pasien</th>
-            <th scope="col">Masuk</th>
-            <th scope="col">Keluar</th>
-            <th scope="col">Jenis Berobat</th>
-            <th scope="col">Dokter</th>
-            <th scope="col">Ruangan</th>
-            <th scope="col">Penyakit</th>
+                <th scope="col">Nama Pasien</th>
+                <th scope="col">Masuk</th>
+                <th scope="col">Keluar</th>
+                <th scope="col">Jenis Berobat</th>
+                <th scope="col">Dokter</th>
+                <th scope="col">Ruangan</th>
+                <th scope="col">Penyakit</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-            <td><?=$pasien->nama; ?></td>
-            <td><?=$berobat->tgl_masuk; ?></td>
-            <td><?=$berobat->tgl_keluar; ?></td>
-            <td><?=$berobat->jenis_berobat; ?></td>
-            <td><?=$pegawai->nama; ?></td>
-            <td><?=$ruangan->nama; ?></td>
-            <td><?=$penyakit->nama; ?></td>
+                <td><?= $pasien->nama; ?></td>
+                <td><?= $berobat->tgl_masuk; ?></td>
+                <td><?= $berobat->tgl_keluar; ?></td>
+                <td><?= $berobat->jenis_berobat; ?></td>
+                <td><?= $pegawai->nama; ?></td>
+                <td><?= $ruangan->nama; ?></td>
+                <td><?= $penyakit->nama; ?></td>
             </tr>
         </tbody>
     </table>
@@ -98,28 +99,28 @@ if ($trx!=NULL) {
         </thead>
         <tbody>
             <?php $totalharga = 0 ?>
-            <?php for ($i=0; $i < count($trx); $i++) : ?>
+            <?php for ($i = 0; $i < count($trx); $i++) : ?>
                 <tr>
-                    <?php $ob = Obat::findOne(['id_obat'=>$trx[$i]->id_obat]); ?>
-                    <?php $td = Tindakan::findOne(['id_tindakan'=>$trx[$i]->id_tindakan]); ?>
-                    <th scope="row"><?=$i+1; ?></th>
-                    <td><?php if($ob != NULL) : ?>
-                        <?= $ob->nama; ?>
-                        <?php endif;?>
+                    <?php $ob = Obat::findOne(['id_obat' => $trx[$i]->id_obat]); ?>
+                    <?php $td = Tindakan::findOne(['id_tindakan' => $trx[$i]->id_tindakan]); ?>
+                    <th scope="row"><?= $i + 1; ?></th>
+                    <td><?php if ($ob != NULL) : ?>
+                            <?= $ob->nama; ?>
+                        <?php endif; ?>
                     </td>
-                    <td><?php if($td != NULL) : ?>
-                        <?= $td->nama; ?>
-                        <?php endif;?>
+                    <td><?php if ($td != NULL) : ?>
+                            <?= $td->nama; ?>
+                        <?php endif; ?>
                     </td>
-                    <td><?= number_format($trx[$i]->harga,0,',','.'); ?></td>
+                    <td><?= number_format($trx[$i]->harga, 0, ',', '.'); ?></td>
                 </tr>
                 <?php $totalharga += $trx[$i]->harga; ?>
             <?php endfor; ?>
             <tr>
-                <th scope="col" colspan = 3>Total</th>
-                <th scope="col"><?= number_format($totalharga,0,',','.'); ?></th>
+                <th scope="col" colspan=3>Total</th>
+                <th scope="col"><?= number_format($totalharga, 0, ',', '.'); ?></th>
             </tr>
-        </tbody>                                        
+        </tbody>
     </table>
 
 </div>
